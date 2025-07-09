eea9459ecda3b6ad3d76b3a9f3fb0fc00e5eb4e6
webdep24h <webdep24h.com@gmail.com>
2025-07-09 14:31:22 +0700

    v2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/QTHome-Construction/', // Đây là bắt buộc!
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
